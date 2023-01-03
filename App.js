62a7cbdba361a9990c8ff448fe7243e6e6167456
Paulo <paulo.vic.per@gmail.com>
2023-01-02 22:15:10 -0300

    1-app-paul

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>GAME REG</Text>
+      <Text>Registre seus jogos facilmente</Text>
+      <Text>Escolha seu console</Text>
+      <Text>Sony</Text>
+      <Text>Nintendo</Text>
+      <Text>Microsoft</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: '#fff',
+    fontSize: 28,
+    marginBottom: 50,
+  }
+
 });
