e4243820cf2c663701697f6ade80c5732e11c2c6
Paulo <paulo.vic.per@gmail.com>
2023-01-08 21:02:38 -0300

    1-app-paul

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, Button, Alert, Separator } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+          source={require('./assets/game-controller-outline.png')}
+          style={styles.logo}
+          />
       <Text style={styles.title}>GAME REG</Text>
-      <Text>Registre seus jogos facilmente</Text>
-      <Text>Escolha seu console</Text>
-      <Text>Sony</Text>
-      <Text>Nintendo</Text>
-      <Text>Microsoft</Text>
+      <Text style={styles.texto}>Registre seus jogos facilmente</Text>  
+        <Button
+          style={styles.botao}
+          title="Escolha seu console"
+          onPress={()=>Alert.alert("Atenção","Escolha uma das opções")}
+          color="black"
+          />
+      <Text style={styles.texto2}>Sony</Text>
+      <Text style={styles.texto2}>Nintendo</Text>
+      <Text style={styles.texto2}>Microsoft</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,15 +27,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#4682B4',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
+  logo: {
+    width: 200,
+    height: 200,
+  },
+
   title: {
-    color: '#fff',
-    fontSize: 28,
-    marginBottom: 50,
-  }
+    color: '#C0C0C0',
+    fontSize: 40,
+    marginBottom: 20,
+  },
+
+  texto: {
+    color: '#C0C0C0',
+    marginBottom: 20,
+  },
+
+  texto2: {
+    color: '#C0C0C0',
+    marginTop: 20,
+  },
 
 });
